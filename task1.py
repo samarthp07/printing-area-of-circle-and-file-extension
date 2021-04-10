68caa23a467a8548edd4b60887ad520b052392fd
samarthp07 <82090424+samarthp07@users.noreply.github.com>
2021-04-10 19:24:09 +0530

    Uploaded by Samarth penshanwar
    This program prints area of circle from radius...
    And second program prints extension of file

---
+++ task1.py
@@ -0,0 +1,18 @@
+print ("myCaptain Python task 1")
+
+print("*-*-*-*-*-*-*-*-*-*-*-*")
+print ("\nCode for:- Area of circle!")
+print ("--------------------------")
+radius = input("Enter radius of circle:- ")
+radius = float (radius)
+print ("radius:=" , radius)
+print ("Area of circle:-", 3.15*radius*radius )
+print ("\n************************")
+
+
+print ("\n\nCode for:- Printing extension of enterd file name !")
+print ("--------------------------------------------------")
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+print ("\n***********************")
